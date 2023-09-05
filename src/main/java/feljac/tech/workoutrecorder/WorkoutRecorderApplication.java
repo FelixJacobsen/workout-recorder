757dd921cc3b3cb0ab6692d252fbb3aa562fc302
FelixJacobsen <felix.jacobsen@iths.se>
2023-09-05 22:25:48 +0200

    initial commit

---
+++ src/main/java/feljac/tech/workoutrecorder/WorkoutRecorderApplication.java
@@ -0,0 +1,13 @@
+package feljac.tech.workoutrecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutRecorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutRecorderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/feljac/tech/workoutrecorder/WorkoutRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package feljac.tech.workoutrecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutRecorderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
